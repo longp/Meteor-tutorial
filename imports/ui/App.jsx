62640564a74e80d092df0625e615ca3dd036b8f3
longp <lphan1989@gmail.com>
2016-09-14 23:15:20 -0400

    checked box and remove tasks owrks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,6 +8,13 @@ import Task from './Task.jsx';
 
 // App component - represents the whole app
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hideCompleted: false,
+    };
+  }
   handleSubmit(event) {
     event.preventDefault();
 
@@ -20,9 +27,18 @@ class App extends Component {
     ReactDom.findDOMNode(this.refs.textInput).value = '';
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    })
+  }
 
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked)
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -32,6 +48,17 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide completed tasks
+          </label>
+
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input type="text" ref="textInput" placeholder="Type to add new tasks"/>
           </form>
